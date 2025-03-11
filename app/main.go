2034b951640c816f12cb220ffc7bd8f7e2761cef
Sandeep Kumar <sandepten@gmail.com>
2025-03-11 12:47:14 +0530

    mutliple ping

--- app/main.go
+++ app/main.go
@@ -18,10 +18,16 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	conn.Write([]byte("+PONG\r\n"))
+	defer conn.Close()
+
+	_, err = conn.Write([]byte("+PONG\r\n+PONG\r\n"))
+	if err != nil {
+		fmt.Println("Unable to write", err)
+	}
 }
